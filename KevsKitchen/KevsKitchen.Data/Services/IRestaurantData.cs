6fb9df2141ae53d033277cac11b3fbc38b98f037
yeyuans <ysong@conservice.com>
2019-09-20 11:54:41 -0600

    create data locally

---
+++ KevsKitchen/KevsKitchen.Data/Services/IRestaurantData.cs
@@ -0,0 +1,13 @@
+﻿using KevsKitchen.Data.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KevsKitchen.Data.Services
+{
+    public interface IRestaurantData
+    {
+        IEnumerable<Restaurant> GetAll();
+    }
+}
